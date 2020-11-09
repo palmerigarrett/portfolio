bf1da9a07f3b088ad4fd2795b6f2f8e791362d7f
palmerigarrett <palmerigarrett@gmail.com>
2020-11-09 12:16:19 -0500

    Updates

---
+++ precache-manifest.c8818d52b0e1e778a7afb7c815b6de7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da9e91203b840bdcf0c3b6a9778bcad",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a9f6267370f49eeacd5e",
+    "url": "/portfolio/static/css/main.195d287a.chunk.css"
+  },
+  {
+    "revision": "1e3bb162237e6b380e17",
+    "url": "/portfolio/static/js/2.74e0ddae.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.74e0ddae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7787ff4240e46f02b7",
+    "url": "/portfolio/static/js/3.e88ba757.chunk.js"
+  },
+  {
+    "revision": "a9f6267370f49eeacd5e",
+    "url": "/portfolio/static/js/main.ac2e289b.chunk.js"
+  },
+  {
+    "revision": "e22c20a71ec5bb5775a0",
+    "url": "/portfolio/static/js/runtime-main.f2c9d485.js"
+  }
+]);
